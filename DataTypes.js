b7488fd35a52c8d054259ae7a29264ff84f74904
ARVIND ARYA <113885816+ArvindArya2002@users.noreply.github.com>
2025-09-26 23:55:20 +0530

    dataTypes

---
+++ DataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treats all JS code as newer version
+// alert("hello") // we are using node js,not browser
+// number
+// bigint
+// string
+// boolean
+// null=>standalone value
+// undefined
+// symbol => unique
+console.log(typeof undefined)// undefined
+console.log(typeof null);//object
+
