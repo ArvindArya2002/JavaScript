63c8b13e4fa10368afe7a42c1dde5197d48ea513
ARVIND ARYA <113885816+ArvindArya2002@users.noreply.github.com>
2025-09-26 22:42:00 +0530

    Basics

---
+++ variables.js
@@ -0,0 +1,6 @@
+const studentId=101
+let studentName="Arvind"
+var studentAddress="Vill Saluni"
+studentCon="997745"
+let studentYear
+console.table([studentId,studentName,studentAddress,studentCon,studentYear])
